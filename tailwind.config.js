7a7c1a64c4dc85f835b342da5f08aeabe9433e21
Imamhussain1 <about.imamhussain@gmail.com>
2025-06-25 12:31:04 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      "brand-green": "#28A745",
+    },
+  },
+  plugins: [],
+};
